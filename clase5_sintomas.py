4cfd4adbd3059dc2cb8d15bef1473aeea87327bf
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2022-03-24 12:10:49 -0500

    apuntes del curso introducción al pensamiento probabilístico de platzi

---
+++ clase5_sintomas.py
@@ -0,0 +1,20 @@
+"""
+prior_A = 
+prob_B_dado_A = probabilidad de que tengamos evidencia dado que la hipotesis sea cierta
+prob_B = cuál es la probabilidad de B o de la evidencia o el evento
+"""
+
+def calc_bayes(prior_A, prob_B_dado_A, prob_B):
+    return (prior_A * prob_B_dado_A ) / prob_B
+
+if __name__ == '__main__':
+    prob_cancer = 1/100000
+    prob_sintoma_dado_cancer = 1
+    prob_sintoma_dado_no_cancer = 10 / 99999
+    prob_no_cancer = 1 - prob_cancer
+
+    prob_sintoma = (prob_sintoma_dado_cancer * prob_cancer) + (prob_sintoma_dado_no_cancer * prob_no_cancer)
+    print(prob_sintoma)
+
+    prob_cancer_dado_sintoma = calc_bayes(prob_cancer, prob_sintoma_dado_cancer, prob_sintoma)
+    print(prob_cancer_dado_sintoma)
